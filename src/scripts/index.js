19802d98122e92dfbdc9f8f1983f023bfb42799a
Poveii <yellowtutors@gmail.com>
2022-08-04 16:08:41 -0300

    Infinite Slider Feature

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,22 +6,34 @@ const cards = document.querySelectorAll(".card");
 let indexCards = 0;
 
 /* ======== FUNCTIONS ======== */
-function hideCardSelected() {
+function hideCurrentCard() {
   const selectedCard = document.querySelector(".selected");
   selectedCard.classList.remove("selected");
 }
 
 forwardButton.addEventListener("click", function () {
-  if (indexCards === cards.length - 1) return; // -1 because cards.length returns 3 instead 2 (Counting by 1)
+  if (indexCards === cards.length - 1) {
+    hideCurrentCard();
+
+    indexCards = 0;
+    cards[indexCards].classList.add("selected");
+    return;
+  } // -1 cause cards.length returns 3 instead 2
   indexCards++;
   cards[indexCards].classList.add("selected");
 
-  hideCardSelected();
+  hideCurrentCard();
 });
 
 backwardButton.addEventListener("click", function () {
-  if (indexCards === 0) return;
-  hideCardSelected();
+  if (indexCards === 0) {
+    indexCards = cards.length - 1;
+    hideCurrentCard();
+
+    cards[indexCards].classList.add("selected");
+    return;
+  }
+  hideCurrentCard();
 
   indexCards--;
   cards[indexCards].classList.add("selected");
