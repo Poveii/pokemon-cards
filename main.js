1d87028cce3bcc04ee2f341aa1cc36c6bc4ae23b
Poveii <yellowtutors@gmail.com>
2022-09-24 19:04:37 -0300

    :bug: fix: Repeated Pokemon Card

--- main.js
+++ main.js
@@ -1,37 +1,11 @@
+const limit = 905;
+
 function randomNumberGenerator(array) {
   return Math.floor(Math.random() * array);
 }
 
-let randomNumber = randomNumberGenerator(906);
+let randomNumber = randomNumberGenerator(limit);
 // console.log(randomNumber);
-
-backwardButton.addEventListener("click", () => {
-  if (randomNumber <= 1) {
-    randomNumber = 906;
-    setPokemonCard();
-    return;
-  }
-  randomNumber--;
-
-  setPokemonCard();
-});
-
-forwardButton.addEventListener("click", () => {
-  if (randomNumber >= 906) {
-    randomNumber = 1;
-    setPokemonCard();
-    return;
-  }
-  randomNumber++;
-
-  setPokemonCard();
-});
-
-refreshButton.addEventListener("click", () => {
-  randomNumber = randomNumberGenerator(906);
-
-  setPokemonCard();
-});
 
 function neverReturnZero(number) {
   if (number == 0) number++;
@@ -61,80 +35,120 @@ const pokemonTypesList = [
   { name: "Shadow", color: "#281525", textColor: "#FFFFFF" },
 ];
 
-function setPokemonCard() {
-  let randomNumberToBackground = randomNumberGenerator(5);
+fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`)
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data.results);
 
-  randomNumber = neverReturnZero(randomNumber);
+    backwardButton.addEventListener("click", () => {
+      const lastPokemonIdentity = pokemonIdentity.textContent;
 
-  const url = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((pokemon) => {
-      // console.log(pokemon);
-
-      pokemonImage.classList.remove("opacity");
-
-      randomNumberToBackground = neverReturnZero(randomNumberToBackground);
-
-      pokemonBackground.className = `card background-${randomNumberToBackground}`;
-
-      pokemonImage.src = pokemon.sprites.other.home.front_default;
-
-      pokemonName.textContent =
-        pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
-
-      pokemonHealth.textContent = pokemon.stats[0].base_stat;
-
-      pokemonIdentity.textContent = randomNumber;
-
-      pokemonType.textContent =
-        pokemon.types[0].type.name[0].toUpperCase() +
-        pokemon.types[0].type.name.substring(1);
-
-      pokemonTypesList.forEach((type) => {
-        if (type.name != pokemonType.textContent) return;
-
-        typeColor.style.backgroundColor = type.color;
-
-        pokemonType.style.color = type.textColor;
+      if (lastPokemonIdentity <= 1) {
+        randomNumber = limit;
+        setPokemonCard(data.results[randomNumber].url);
         return;
-      });
+      }
+      randomNumber--;
 
-      pokemonAbility.textContent =
-        pokemon.abilities[0].ability.name[0].toUpperCase() +
-        pokemon.abilities[0].ability.name.substring(1);
-
-      fetch(pokemon.abilities[0].ability.url)
-        .then((response) => response.json())
-        .then((ability) => {
-          // console.log(ability);
-
-          ability.effect_entries.forEach((item) => {
-            if (item.language.name != "en") return;
-
-            abilityText.textContent = item.effect;
-          });
-        });
-
-      pokemonMove.textContent =
-        pokemon.moves[0].move.name[0].toUpperCase() +
-        pokemon.moves[0].move.name.substring(1);
-
-      fetch(pokemon.moves[0].move.url)
-        .then((response) => response.json())
-        .then((move) => {
-          // console.log(move);
-
-          move.effect_entries.forEach((item) => {
-            if (item.language.name != "en") return;
-
-            moveText.textContent = item.effect;
-          });
-        });
-
-      pokemonImage.classList.add("opacity");
+      setPokemonCard(data.results[randomNumber].url);
     });
-}
 
-setPokemonCard();
+    forwardButton.addEventListener("click", () => {
+      const lastPokemonIdentity = pokemonIdentity.textContent;
+
+      if (lastPokemonIdentity >= limit) {
+        randomNumber = 1;
+        setPokemonCard(data.results[randomNumber].url);
+        return;
+      }
+      randomNumber++;
+
+      setPokemonCard(data.results[randomNumber].url);
+    });
+
+    refreshButton.addEventListener("click", () => {
+      const lastPokemonIdentity = pokemonIdentity.textContent;
+
+      randomNumber = randomNumberGenerator(limit);
+
+      const newPokemonIdentity = data.results.filter(
+        (p) => p.url.substring(34).replace("/", "") != lastPokemonIdentity
+      )[randomNumber];
+
+      setPokemonCard(newPokemonIdentity.url);
+    });
+
+    function setPokemonCard(url) {
+      let randomNumberToBackground = randomNumberGenerator(5);
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((pokemon) => {
+          // console.log(pokemon);
+
+          pokemonImage.classList.remove("opacity");
+
+          randomNumberToBackground = neverReturnZero(randomNumberToBackground);
+
+          pokemonBackground.className = `card background-${randomNumberToBackground}`;
+
+          pokemonImage.src = pokemon.sprites.other.home.front_default;
+
+          pokemonName.textContent =
+            pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+
+          pokemonHealth.textContent = pokemon.stats[0].base_stat;
+
+          pokemonIdentity.textContent = pokemon.id;
+
+          pokemonType.textContent =
+            pokemon.types[0].type.name[0].toUpperCase() +
+            pokemon.types[0].type.name.substring(1);
+
+          pokemonTypesList.forEach((type) => {
+            if (type.name != pokemonType.textContent) return;
+
+            typeColor.style.backgroundColor = type.color;
+
+            pokemonType.style.color = type.textColor;
+            return;
+          });
+
+          pokemonAbility.textContent =
+            pokemon.abilities[0].ability.name[0].toUpperCase() +
+            pokemon.abilities[0].ability.name.substring(1);
+
+          fetch(pokemon.abilities[0].ability.url)
+            .then((response) => response.json())
+            .then((ability) => {
+              // console.log(ability);
+
+              ability.effect_entries.forEach((item) => {
+                if (item.language.name != "en") return;
+
+                abilityText.textContent = item.effect;
+              });
+            });
+
+          pokemonMove.textContent =
+            pokemon.moves[0].move.name[0].toUpperCase() +
+            pokemon.moves[0].move.name.substring(1);
+
+          fetch(pokemon.moves[0].move.url)
+            .then((response) => response.json())
+            .then((move) => {
+              // console.log(move);
+
+              move.effect_entries.forEach((item) => {
+                if (item.language.name != "en") return;
+
+                moveText.textContent = item.effect;
+              });
+            });
+
+          pokemonImage.classList.add("opacity");
+        });
+    }
+
+    setPokemonCard(data.results[randomNumber].url);
+  });
